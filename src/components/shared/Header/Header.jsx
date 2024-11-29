e98e6074f08403c545f0c852f310fd44b53b0a26
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-11-29 19:29:16 +0600

    font changed and code refactor

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -21,140 +21,148 @@ import { IoIosArrowDown } from "react-icons/io";
 import logo from "/public/flytech-White.png";
 
 const Header = () => {
-  const courses = [
-    {
-      name: "Web Design & Development with PHP, LARAVEL",
-      link: "",
-      logo: laravelLogo,
-    },
-    {
-      name: "MERN Stack, Next Js",
-      link: "",
-      logo: mernLogo,
-    },
-    {
-      name: "Cyber Security & Ethical Hacking",
-      link: "",
-      logo: cyberLogo,
-    },
-    {
-      name: "Programming With Python",
-      link: "",
-      logo: pyhtonLogo,
-    },
-    {
-      name: "Python & Django, DevOps Web & Software Development",
-      link: "",
-      logo: dJangoLogo,
-    },
-    {
-      name: "Python Software developer With DevOps",
-      link: "",
-      logo: devOpsLogo,
-    },
-    {
-      name: "Flutter Mobile Apps Development",
-      link: "",
-      logo: flutterLogo,
-    },
-    {
-      name: "Graphics Design",
-      link: "",
-      logo: graphicsLogo,
-    },
-    {
-      name: "Basic Computer & Office Job, Data Entry",
-      link: "",
-      logo: basicLogo,
-    },
-    {
-      name: "Social Media Marketing",
-      link: "",
-      logo: marketingLogo,
-    },
-    {
-      name: "Website SEO Basic To Advanced",
-      link: "",
-      logo: seoLogo,
-    },
-    {
-      name: "Wordpress",
-      link: "",
-      logo: wordpressLogo,
-    },
-    {
-      name: "UX/UI Design",
-      link: "",
-      logo: uiuxLogo,
-    },
-  ];
+    const courses = [
+        {
+            name: "Web Design & Development with PHP, LARAVEL",
+            link: "",
+            logo: laravelLogo,
+        },
+        {
+            name: "MERN Stack, Next Js",
+            link: "",
+            logo: mernLogo,
+        },
+        {
+            name: "Cyber Security & Ethical Hacking",
+            link: "",
+            logo: cyberLogo,
+        },
+        {
+            name: "Programming With Python",
+            link: "",
+            logo: pyhtonLogo,
+        },
+        {
+            name: "Python & Django, DevOps Web & Software Development",
+            link: "",
+            logo: dJangoLogo,
+        },
+        {
+            name: "Python Software developer With DevOps",
+            link: "",
+            logo: devOpsLogo,
+        },
+        {
+            name: "Flutter Mobile Apps Development",
+            link: "",
+            logo: flutterLogo,
+        },
+        {
+            name: "Graphics Design",
+            link: "",
+            logo: graphicsLogo,
+        },
+        {
+            name: "Basic Computer & Office Job, Data Entry",
+            link: "",
+            logo: basicLogo,
+        },
+        {
+            name: "Social Media Marketing",
+            link: "",
+            logo: marketingLogo,
+        },
+        {
+            name: "Website SEO Basic To Advanced",
+            link: "",
+            logo: seoLogo,
+        },
+        {
+            name: "Wordpress",
+            link: "",
+            logo: wordpressLogo,
+        },
+        {
+            name: "UX/UI Design",
+            link: "",
+            logo: uiuxLogo,
+        },
+    ];
 
-  const [isDropdownClicked, setIsDropdownClicked] = useState(false);
-  return (
-    <header className="py-4 border-b border-gray-700 backdrop:blur relative">
-      <div className="flex justify-between items-center max-width">
-        <div>
-          <Image src={logo} alt="Flytech It" className="w-40" />
-        </div>
-        <nav>
-          <ul className="flex gap-4 font-light">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">About</Link>
-            </li>
-            <li>
-              <Link href="/services">Services</Link>
-            </li>
-            <li
-              className=" relative"
-              onClick={() => setIsDropdownClicked(!isDropdownClicked)}
-            >
-              <span className="flex items-center gap-2 cursor-pointer">
-                Courses <IoIosArrowDown />
-              </span>
-              <ul
-                className={`${
-                  isDropdownClicked
-                    ? "opacity-100 visible"
-                    : "invisible opacity-0"
-                } duration-300 grid gap-5 grid-cols-3 w-[800px] absolute z-10 bg-black border border-gray-700 p-4 rounded-sm top-[40px] left-1/2 -translate-x-1/2 backdrop:blur-sm bg-gradient-to-r from-[#030014] to-[#0A001F]`}
-              >
-                {courses?.map((course, index) => (
-                  <li key={index} className="flex items-center gap-2">
-                    <Image
-                      src={course?.logo}
-                      alt={course?.name}
-                      className="w-10"
-                    />
-                    <Link href={course?.link} className="font-semibold text-sm">
-                      {course?.name}
+    const [isDropdownClicked, setIsDropdownClicked] = useState(false);
+    return (
+        <header className="py-4 border-b border-gray-700/50 backdrop:blur relative">
+            <div className="flex justify-between items-center max-width">
+                <div>
+                    <Image src={logo} alt="Flytech It" className="w-40" />
+                </div>
+                <nav>
+                    <ul className="flex gap-6 font-medium text-base">
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/services">Services</Link>
+                        </li>
+                        <li
+                            className=" relative"
+                            onClick={() =>
+                                setIsDropdownClicked(!isDropdownClicked)
+                            }
+                        >
+                            <span className="flex items-center gap-2 cursor-pointer ">
+                                Courses <IoIosArrowDown/>
+                            </span>
+                            <ul
+                                className={`${
+                                    isDropdownClicked
+                                        ? "opacity-100 visible"
+                                        : "invisible opacity-0"
+                                } duration-300 grid gap-5 grid-cols-3 w-[800px] absolute z-10 bg-black border border-gray-700 p-4 rounded-sm top-[40px] left-1/2 -translate-x-1/2 backdrop:blur-sm bg-gradient-to-r from-[#030014] to-[#0A001F]`}
+                            >
+                                {courses?.map((course, index) => (
+                                    <li
+                                        key={index}
+                                        className="flex items-center gap-2"
+                                    >
+                                        <Image
+                                            src={course?.logo}
+                                            alt={course?.name}
+                                            className="w-10"
+                                        />
+                                        <Link
+                                            href={course?.link}
+                                            className="font-semibold text-sm"
+                                        >
+                                            {course?.name}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                        <li>
+                            <Link href="/portfolio">Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="flex items-center">
+                    <Link
+                        href={"https://facebook.com/flytechitbd"}
+                        target="_blank"
+                        className="size-8 rounded-full border-2 flex items-center justify-center"
+                    >
+                        <FaFacebookF />
                     </Link>
-                  </li>
-                ))}
-              </ul>
-            </li>
-            <li>
-              <Link href="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex items-center">
-          <Link
-            href={"https://facebook.com/flytechitbd"}
-            target="_blank"
-            className="size-8 rounded-full border-2 flex items-center justify-center"
-          >
-            <FaFacebookF />
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
