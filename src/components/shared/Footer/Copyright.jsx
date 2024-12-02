935325409d331afe944daa89ad5c67b990a88e12
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-12-02 21:42:55 +0600

    border added in footer

--- src/components/shared/Footer/Copyright.jsx
+++ src/components/shared/Footer/Copyright.jsx
@@ -1,10 +1,12 @@
 const Copyright = () => {
-  return (
-    <div className="max-width text-center pt-10 pb-5 text-sm font-mono">
-      Copyright &copy; 2024, designed & developed by{" "}
-      <span className="text-blue-500 font-semibold">FlyTech IT</span>
-    </div>
-  );
+    return (
+        <div className="pt-4 pb-5  border-t border-white/10">
+            <p className="max-width text-center text-sm font-mono">
+                Copyright &copy; 2024, designed & developed by{" "}
+                <span className="text-blue-500 font-semibold">FlyTech IT</span>
+            </p>
+        </div>
+    );
 };
 
 export default Copyright;
