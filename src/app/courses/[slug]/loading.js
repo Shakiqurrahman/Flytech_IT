dcd56e5580ca359edaae5238a191ea3dafd18585
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-12-10 22:24:28 +0600

    skeleton responsive

--- src/app/courses/[slug]/loading.js
+++ src/app/courses/[slug]/loading.js
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const loading = () => {
     return (
-        <div className="max-width flex gap-5 mt-10 min-h-screen">
+        <div className="max-width md:flex gap-5 mt-10 min-h-screen">
             <div className="w-full md:w-[calc(100%_-_380px)] h-full mb-2">
                 <Skeleton className="w-full h-[450px]" />
                 <div className="space-y-8 mt-4 w-full">
@@ -11,7 +11,7 @@ const loading = () => {
                     <Skeleton className="h-12 w-full" />
                 </div>
             </div>
-            <div className="w-[380px]">
+            <div className="w-full md:w-[380px]">
                 <Skeleton className="h-[450px] w-full" />
                 <Skeleton className="h-[110px] w-full mt-4" />
             </div>
