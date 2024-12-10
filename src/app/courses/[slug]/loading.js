4462910184f2e0aa12491a4b43a55c6417dea20b
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-12-10 22:30:04 +0600

    responsive bug fixed

--- src/app/courses/[slug]/loading.js
+++ src/app/courses/[slug]/loading.js
@@ -2,13 +2,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const loading = () => {
     return (
-        <div className="max-width md:flex gap-5 mt-10 min-h-screen">
+        <div className="max-width flex flex-col md:flex-row gap-5 mt-10 min-h-screen">
             <div className="w-full md:w-[calc(100%_-_380px)] h-full mb-2">
-                <Skeleton className="w-full h-[450px]" />
+                <Skeleton className="w-full h-[200px] sm:h-[450px]" />
                 <div className="space-y-8 mt-4 w-full">
                     <Skeleton className="h-10 w-full text-2xl sm:text-3xl mt-5" />
-                    <Skeleton className="h-32 w-full" />
-                    <Skeleton className="h-12 w-full" />
+                    <Skeleton className="h-24 sm:h-32 w-full" />
+                    <Skeleton className="hidden md:block h-12 w-full" />
                 </div>
             </div>
             <div className="w-full md:w-[380px]">
