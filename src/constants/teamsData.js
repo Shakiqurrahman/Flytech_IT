0863ef1706b1f0b76d81f4d4e4d98669591bdcc2
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-04-24 17:47:02 +0600

    Team Members and Course Module Updated

--- src/constants/teamsData.js
+++ src/constants/teamsData.js
@@ -1,6 +1,13 @@
 import CEO from "/public/images/about-us/team-member/Flytech-IT-CEO-Mokbul-Hussain.jpg";
+import ComputerOperator from "/public/images/about-us/team-member/Flytech-IT-Computer-Operator.jpg";
 import DeveloperShakil from "/public/images/about-us/team-member/Flytech-IT-Developer-Shakil.png";
+import DigitalMarketerAfzal from "/public/images/about-us/team-member/Flytech-IT-Digital-Marketer-Afzal.jpg";
+import DigitalMarketerMahim from "/public/images/about-us/team-member/Flytech-IT-Digital-Marketer-Mahim.jpg";
+import GraphicsDesigner from "/public/images/about-us/team-member/Flytech-IT-Graphics-Designer.jpg";
+import HrDirector from "/public/images/about-us/team-member/Flytech-IT-HR-Director.jpeg";
 import DeveloperMahdi from "/public/images/about-us/team-member/Flytech-IT-MERN-DEVELOPER.jpeg";
+import UiUxDesigner from "/public/images/about-us/team-member/Flytech-IT-Ui-Ux-Designer.jpeg";
+import WebDesigner from "/public/images/about-us/team-member/Flytech-IT-Web-Designer.jpeg";
 
 export const teamsData = [
   {
@@ -20,8 +27,64 @@ export const teamsData = [
   {
     id: 3,
     image: DeveloperMahdi,
+    name: "Shakiqur Rahman",
+    title: "MERN Stack Developer",
+    bio: "I'm passionate about creating efficient and scalable solutions, and always eager to learn new technologies.",
+  },
+  {
+    id: 4,
+    image: DeveloperMahdi,
     name: "Mahdi Hussain",
     title: "MERN Stack Developer",
     bio: "I'm passionate about creating efficient and scalable solutions, and always eager to learn new technologies.",
+  },
+  {
+    id: 5,
+    image: HrDirector,
+    name: "Ahsanul Haque Chawdhury",
+    title: "HR and Creative Director",
+    bio: "With a dynamic blend of strategic leadership and creative vision, leads FlyTech IT’s human resources and creative initiatives.",
+  },
+  {
+    id: 6,
+    image: ComputerOperator,
+    name: "Atikur Rahman",
+    title: "Computer Operator",
+    bio: "Dedicated Computer Operator with expertise in system monitoring, data management, and technical support to ensure smooth and efficient IT operations.",
+  },
+  {
+    id: 7,
+    image: DigitalMarketerMahim,
+    name: "Masud Hasan Mahim",
+    title: "Digital Marketer",
+    bio: "Digital Marketer at Flytech IT, crafting data-driven, result-oriented marketing strategies using modern digital channels to deliver impactful, user-focused brand experiences.",
+  },
+  {
+    id: 7,
+    image: GraphicsDesigner,
+    name: "MS Rima Akter",
+    title: "Graphics Designer",
+    bio: "I'm passionate grapchis designer who transforms ideas into inpactful visuals. Skilled in Illustrator, Photoshop, and InDesign, I create eye-catching designs with purpose. From branding to social media graphics - I design to make stand out.",
+  },
+  {
+    id: 8,
+    image: UiUxDesigner,
+    name: "Gulam Kibria",
+    title: "Ui/Ux Designer",
+    bio: "Passionate UI/UX Designer focused on creating intuitive, user-centered designs that enhance user experience and bridge the gap between users and technology.",
+  },
+  {
+    id: 9,
+    image: WebDesigner,
+    name: "Shumon Ahmad",
+    title: "Web Designer",
+    bio: "Creative Web Designer skilled in crafting user-friendly, responsive websites with a strong eye for detail, visual design, and seamless user experience.",
+  },
+  {
+    id: 10,
+    image: DigitalMarketerAfzal,
+    name: "Afzal Hussain",
+    title: "Digital Marketer",
+    bio: "Results-driven Digital Marketer specializing in SEO, social media, and online advertising to boost brand visibility and drive business growth.",
   },
 ];
