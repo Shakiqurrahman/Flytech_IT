dab0d7a722a12bd8ea8f882914bc30bc28e08c79
Shakil Ahmed <itsdevelopershakil@gmail.com>
2025-07-09 01:17:10 +0600

    added team member abid

--- src/constants/teamsData.js
+++ src/constants/teamsData.js
@@ -6,6 +6,7 @@ import DigitalMarketerMahim from "/public/images/about-us/team-member/Flytech-IT
 import GraphicsDesigner from "/public/images/about-us/team-member/Flytech-IT-Graphics-Designer.jpg";
 import HrDirector from "/public/images/about-us/team-member/Flytech-IT-HR-Director.jpeg";
 import DeveloperMahdi from "/public/images/about-us/team-member/Flytech-IT-MERN-DEVELOPER.jpeg";
+import DeveloperAbid from "/public/images/about-us/team-member/Flytech-IT-Python-Developer.jpeg";
 import UiUxDesigner from "/public/images/about-us/team-member/Flytech-IT-Ui-Ux-Designer.jpeg";
 import WebDesigner from "/public/images/about-us/team-member/Flytech-IT-Web-Designer.jpeg";
 import shakiqur from "/public/images/about-us/team-member/shakiqur-web-application developer.webp";
@@ -41,48 +42,55 @@ export const teamsData = [
   },
   {
     id: 5,
+    image: DeveloperAbid,
+    name: "Md Abidul Karim Chowdhury",
+    title: "Python Developer",
+    bio: "Passionate about building scalable web applications using Python, Django, and modern databases to solve real-world business problems.",
+  },
+  {
+    id: 6,
     image: HrDirector,
     name: "Ahsanul Haque Chawdhury",
     title: "HR and Creative Director",
     bio: "With a dynamic blend of strategic leadership and creative vision, leads FlyTech IT’s human resources and creative initiatives.",
   },
   {
-    id: 6,
+    id: 7,
     image: ComputerOperator,
     name: "Atikur Rahman",
     title: "Computer Operator",
     bio: "Dedicated Computer Operator with expertise in system monitoring, data management, and technical support to ensure smooth and efficient IT operations.",
   },
   {
-    id: 7,
+    id: 8,
     image: DigitalMarketerMahim,
     name: "Masud Hasan Mahim",
     title: "Digital Marketer",
     bio: "Digital Marketer at Flytech IT, crafting data-driven, result-oriented marketing strategies using modern digital channels to deliver impactful, user-focused brand experiences.",
   },
   {
-    id: 7,
+    id: 9,
     image: GraphicsDesigner,
     name: "MS Rima Akter",
     title: "Graphics Designer",
     bio: "I'm passionate grapchis designer who transforms ideas into inpactful visuals. Skilled in Illustrator, Photoshop, and InDesign, I create eye-catching designs with purpose. From branding to social media graphics - I design to make stand out.",
   },
   {
-    id: 8,
+    id: 10,
     image: UiUxDesigner,
     name: "Gulam Kibria",
     title: "Ui/Ux Designer",
     bio: "Passionate UI/UX Designer focused on creating intuitive, user-centered designs that enhance user experience and bridge the gap between users and technology.",
   },
   {
-    id: 9,
+    id: 11,
     image: WebDesigner,
     name: "Shumon Ahmad",
     title: "Web Designer",
     bio: "Creative Web Designer skilled in crafting user-friendly, responsive websites with a strong eye for detail, visual design, and seamless user experience.",
   },
   {
-    id: 10,
+    id: 12,
     image: DigitalMarketerAfzal,
     name: "Afzal Hussain",
     title: "Digital Marketer",
